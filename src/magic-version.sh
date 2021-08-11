de604c0ac4070c8dd6406da9c939f0aa1912f7c6
sinkcup <sinkcup@gmail.com>
2021-08-11 13:43:40 +0800

    refactor: common setup for #7

--- src/magic-version.sh
+++ src/magic-version.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-if [ -z "$TAG_NAME" ]
-then
-  MAGIC_VERSION=${BRANCH_NAME//\//-}-"$GIT_COMMIT_SHORT"
+if [ -n "$TAG_NAME" ]; then
+  MAGIC_VERSION=${TAG_NAME//\//-}
 else
-  MAGIC_VERSION="$TAG_NAME"
+  MAGIC_VERSION=${BRANCH_NAME//\//-}-${GIT_COMMIT_SHORT}
 fi
 
 echo "$MAGIC_VERSION"
-if [ -n "$CI_ENV_FILE" ]
-then
-  echo "MAGIC_VERSION=$MAGIC_VERSION" >> "$CI_ENV_FILE"
+if [ -n "$CI_ENV_FILE" ]; then
+  echo "MAGIC_VERSION=$MAGIC_VERSION" >>"$CI_ENV_FILE"
 fi
